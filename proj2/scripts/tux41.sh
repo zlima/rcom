02105b4bd1299ab6e4064a4dd4758450bce66e25
zlima <jlima91@hotmail.com>
2015-11-30 17:46:46 +0000

    tux31 config

--- proj2/scripts/tux41.sh
+++ proj2/scripts/tux41.sh
@@ -5,3 +5,5 @@ route add -net 172.16.41.0/24 gw 172.16.40.254
 route -n
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
+route add -net default gw 172.16.40.254
