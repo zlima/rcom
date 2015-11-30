a4df3830a3b5c816398fb922bf34921d623f2d29
zlima <yoyo@MBP-de-yoyo.lan>
2015-11-30 15:38:44 +0000

    scripts

---
+++ proj2/scripts/tux41.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ifconfig eth0 up
+ifconfig eth0 172.16.40.1
+route add -net 172.16.41.0/24 gw 172.16.40.254
+route -n
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
