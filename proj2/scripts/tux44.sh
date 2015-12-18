8f5b49221c182b81a49bebb66ff64556989a7983
vascof92 <vascof92@users.noreply.github.com>
2015-12-18 03:23:23 +0000

    Update tux44.sh

--- proj2/scripts/tux44.sh
+++ proj2/scripts/tux44.sh
@@ -5,4 +5,4 @@ ifconfig eth1 up
 ifconfig eth1 172.16.y1.253
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-route add -net default gw 172.16.y1.254
+route add -net default gw 172.16.y1.254 eth1
