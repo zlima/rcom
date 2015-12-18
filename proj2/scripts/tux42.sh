da7cbe7e26fa36ffcff45f6050da898d07ea9bf4
vascof92 <vascof92@users.noreply.github.com>
2015-12-18 01:15:01 +0000

    Update tux42.sh

--- proj2/scripts/tux42.sh
+++ proj2/scripts/tux42.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ifconfig eth0 up
-ifconfig eth0 172.16.41.1
-route add -net 172.16.40.0/24 gw 172.16.41.253
+ifconfig eth0 172.16.y1.1/24
+route add -net 172.16.y0.0/24 gw 172.16.y1.253
 route -n
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 
-route add -net default gw 172.16.41.254
+route add -net default gw 172.16.y1.254
